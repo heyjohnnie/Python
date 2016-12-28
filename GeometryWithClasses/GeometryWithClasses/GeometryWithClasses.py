5b6ec8fb2ba83210ef0c9b360d6e1f7e843de04f
J. M. Paz <jm.pazro@gmail.com>
2016-12-28 13:05:32 -0600

    User Input is now a class

--- GeometryWithClasses/GeometryWithClasses/GeometryWithClasses.py
+++ GeometryWithClasses/GeometryWithClasses/GeometryWithClasses.py
@@ -3,20 +3,40 @@ import sys
 
 #Defining the int input method, if the user types a non numerical value,
 #the error will be hanlded without ending the program.
-def userInputAsInt():
-    flag = False
-    valueAsStr = ""
-    valueAsInt = 0
-    while flag == False:
-        valueAsStr = input("Type the value: ")
-        try:
-            valueAsInt = int(valueAsStr)
-            flag = True
-        except:
-            error = sys.exc_info()[0]
-            print(error)
-            print("Invalid input, use numeric values.")
-    return valueAsInt
+class userInput:
+
+    def __init__(self):
+        self.valueAsStr = ""
+        self.valueAsInt = 0
+
+    def inputAsInt(self):
+        while 1:
+            valueAsStr = input("Enter the value: ")
+            try:
+                valueAsInt = int(valueAsStr)
+                break
+            except:
+                error = sys.exc_info()[0]
+                print(error)
+                print("Invalid input, use numerical values.")
+        self.valueAsStr = valueAsStr
+        self.valueAsInt = valueAsInt
+        return valueAsInt
+
+#def userInputAsInt():
+#    flag = False
+#    valueAsStr = ""
+#    valueAsInt = 0
+#    while flag == False:
+#        valueAsStr = input("Type the value: ")
+#        try:
+#            valueAsInt = int(valueAsStr)
+#            flag = True
+#        except:
+#            error = sys.exc_info()[0]
+#            print(error)
+#            print("Invalid input, use numeric values.")
+#    return valueAsInt
 
 #The geometrical figure is defined as an object.
 class parallelogram(object):
@@ -30,9 +50,11 @@ class parallelogram(object):
             turtle.right(360/self.side)
 
 print("Please, enter the number of sides: ")
-numOfSides = userInputAsInt()
+numOfSides = userInput()
+sidesInGeo = numOfSides.inputAsInt()
 print("Please, enter the side lenght: ")
-sideLenght = userInputAsInt()
+sideLenght = userInput()
+lengthInGeo = sideLenght.inputAsInt()
 
-userGeometry = parallelogram(numOfSides, sideLenght)
+userGeometry = parallelogram(sidesInGeo, lengthInGeo)
 userGeometry.geometry()
